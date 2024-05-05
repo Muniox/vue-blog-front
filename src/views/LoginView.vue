09f59a2e1b185edbbda4dc1024f25898196864d9
Muniox <muniox20@gmail.com>
2024-05-05 22:47:43 +0200

    Added new style for login inputs

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -26,18 +26,18 @@ const handleSubmit = async () => {
           autoComplete="off"
           name="email"
           placeholder="Email"
-          class="peer bg-transparent h-10 w-72 rounded-lg text-gray-200 placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
+          class="peer bg-transparent h-12 w-72 rounded-lg placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-blue-400 focus:outline-none focus:border-blue-400 text-xl"
           v-model="email"
         />
         <label
           for="email"
-          class="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
+          class="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-xl peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2.5 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
         >
           Email
         </label>
       </div>
 
-      <label for="password" class="relative border-amber-400">
+      <div class="relative bg-inherit">
         <input
           required
           type="password"
@@ -45,22 +45,24 @@ const handleSubmit = async () => {
           placeholder=" "
           autoComplete="off"
           name="password"
-          class="w-[220px] sm:w-full text-2xl border-2 rounded-lg transition duration-200 focus:border-blue-400 focus:ring-blue-400 myPasswordInput myInput relative z-20 bg-transparent"
+          class="peer bg-transparent h-12 w-72 rounded-lg placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-blue-400 focus:outline-none focus:border-blue-400 text-xl"
           v-model="password"
         />
-        <span
-          class="text-2xl absolute left-3 top-1/2 -translate-y-1/2 transition-all duration-200 px-1 text-gray-400 input-text z-10"
+        <label
+          for="password"
+          class="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-xl peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2.5 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
         >
-          Hasło
-        </span>
-      </label>
+          Password
+        </label>
+      </div>
+
       <button
         type="submit"
         class="rounded-full primary-gradient text-white px-8 py-4 font-semibold text-xl duration-300"
       >
         Zaloguj
       </button>
-      <p class="text-red-600">{err}</p>
+
       <span class="flex flex-col gap-6 sm:flex-row sm:items-center">
         Nie masz jeszcze konta?
 
